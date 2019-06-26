08f0178f91ac7bf4a3d5ecc07c4172fb7dbdd5dd
sp-win <cafelashowerezweb@gmail.com>
2019-06-26 17:42:41 +0900

    Revise graph class

--- vertex.py
+++ vertex.py
@@ -4,13 +4,9 @@ class Vertex:
         self.value = value
         self.edges = {}
 
+    def add_edge(self, vertex, weight=0):
+        # self.edges[vertex] is corresponding edge's value
+        self.edges[vertex] = weight
+
     def get_edges(self):
         return list(self.edges.keys())
-
-
-grand_central = Vertex('Grand Central Station')
-forty_second_street = Vertex('42nd Street Station')
-
-    def add_edge(self, vertex):
-        print("Adding edge to {}".format(vertex))
-        self.edges[vertex] = True
